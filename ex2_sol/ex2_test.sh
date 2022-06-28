e721f8d8a09f135dba417db8a3acf51ca8921257
alonit <alon.itach@mail.huji.ac.il>
2022-06-28 11:41:10 +0300

    wip

--- ex2_sol/ex2_test.sh
+++ ex2_sol/ex2_test.sh
@@ -11,15 +11,14 @@ curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE h
 # run student solution script
 #cd /sol
 chmod +x ./availabilityAgent.sh
-
-#echo "127.0.0.1" > hosts
 ./availabilityAgent.sh &
 
 sleep 25
 
-
 DATA=$(curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=hosts_metrics" -H "Accept: application/csv" --data-urlencode "q=SELECT * FROM \"availability_test\"")
 
+echo "Your test data as found in InfluxDB:"
+echo "$DATA"
 echo
 
 if ! echo "$DATA" | grep -q 'name,tags,time,host,value'; then
@@ -29,15 +28,16 @@ fi
 
 
 if ! echo "$DATA" | grep -q "availability_test"; then
-  >&2 printf "bad measurement name"
+  echo "Bad measurement name. Expected 'availability_test'"
   exit 1
 fi
 
-for i in "127.0.0.1,1" "_gateway,0" "google.com,1" "10.0.0.34,0"
+for i in "127.0.0.1,1" "_gateway,0" "google.com,0" "10.0.0.34,0"
 do
-  RES_LINES=$(echo "$DATA" | grep -c $i)
+  set -- $i
+  RES_LINES=$(echo "$DATA" | grep -q -c $i)
   if ((RES_LINES < 3)); then
-    echo "bad number of tests"
+    echo "Bad test results for $1. Expected at least 3 availability test with result $2."
     exit 1
   fi
 done
