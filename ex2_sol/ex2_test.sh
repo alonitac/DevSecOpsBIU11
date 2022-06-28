448b17f1fb82a70f7a4639e39f190ddc893207c7
alonit <alon.itach@mail.huji.ac.il>
2022-06-28 11:28:45 +0300

    wip

--- ex2_sol/ex2_test.sh
+++ ex2_sol/ex2_test.sh
@@ -20,19 +20,16 @@ sleep 25
 
 DATA=$(curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=hosts_metrics" -H "Accept: application/csv" --data-urlencode "q=SELECT * FROM \"availability_test\"")
 
-kill -9 %1
-kill -9 %2
-
 echo
 
 if ! echo "$DATA" | grep -q 'name,tags,time,host,value'; then
-  printf "Bad db columns. Expected to get 'name,tags,time,host,value' but found \'$DATA\'"
+  >&2 printf "Bad db columns. Expected to get 'name,tags,time,host,value' but found \'$DATA\'"
   exit 1
 fi
 
 
 if ! echo "$DATA" | grep -q "availability_test"; then
-  printf "bad measurement name"
+  >&2 printf "bad measurement name"
   exit 1
 fi
 
@@ -40,7 +37,7 @@ for i in "127.0.0.1,1" "_gateway,0" "google.com,0" "10.0.0.34,0"
 do
   RES_LINES=$(echo "$DATA" | grep -c $i)
   if ((RES_LINES < 3)); then
-    printf "bad number of tests"
+    >&2 printf "bad number of tests"
     exit 1
   fi
 done
