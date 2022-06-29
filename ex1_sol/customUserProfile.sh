634a4218f188f42b7fd9bc4be613442471c98b8a
arielfcotrim <arielforner@gmail.com>
2022-06-29 19:47:21 +0300

    ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,23 @@
+# ....code written here before...
 
+# your changes:
+#!/bin/bash
+#print greeting to console when user logs in
+echo Hello $USER
+
+#check for outdated packages to update
+/usr/lib/update-notifier/apt-check --human-readable
+
+#set variable file, assign .token to variable
+file='.token'
+
+#check if .token exists and its permission levels if exists
+if [[ -f $file && `stat -c %a $file` != 600 ]] ; then
+
+#if true print to console
+echo 'Warning: .token file has too open permissions'
+
+else
+echo 'INFO | .token file has good permission settings.'
+
+fi
