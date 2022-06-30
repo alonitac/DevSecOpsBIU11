37bb76a2affcd8ff831eb06a5f7629c2134ca7ff
Sveta <savrekh@gmail.com>
2022-06-30 19:31:47 +0300

    ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -14,9 +14,5 @@ FILE=$HOME/.token
 # shellcheck disable=SC1073
 # shellcheck disable=SC1072
 # shellcheck disable=SC1009
-if test -f "$FILE" &&  stat -c '%a' -ne 600  ;
-  then
-  echo "Warning: .token file has too open permissions" ;
-fi
-
+if `test -f $FILE` && [ `stat -c "%a" $FILE` != 600 ] ; then echo 'Warning: .token file has too open permissions' ; fi
 
