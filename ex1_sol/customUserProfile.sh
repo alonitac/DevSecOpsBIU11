5bcdaa44b0b1e3f1183f9ea7762792b79a1241ad
simon <simonshmo@gmail.com>
2022-06-30 21:21:44 +0300

    ex1_test1

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 echo "****Hello $USER****"
-echo `/usr/lib/update-notifier/apt-check --human-readable`
+echo`/usr/lib/update-notifier/apt-check --human-readable`
 TOKEN=600
 FILE=~/.token
 PER=$(sudo stat -c '%a' $FILE)
-if [ -f $FILE ]
-then
-  echo "**File Exist**"
 
-else
-  echo "**File Not Exist**"
-fi
-
-if [ ! "$PER" -eq "$TOKEN" ]
+if [[ -f $FILE && "$PER" -ne "600" ]]
 then
-  echo ""Warning: .token file has too open permissions""
+  echo "**Warning: .token file has too open permissions**"
 fi
