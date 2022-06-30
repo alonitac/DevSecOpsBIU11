a88675c91f4cdc99f9ed70b60328233bf8c574f5
JonathanL <yonilevi0013@gmail.com>
2022-06-30 16:42:40 +0300

    WIP

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,15 +1,25 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
 
 #HOST_NAME_IP=${awk 'NR==i {print $1}' /etc/hosts}
 
+HOSTNAME_IP=""
+RESULT=""
+PING_RESULTS=""
+NUM=0
+TIME_NS=0
 
 while true
 do
-    while [[ $HOST_NAME_IP != "" ]]
+    while [[ $HOSTNAME_IP != "" ]]
     do
-      awk 'NR==i {print $1}' /etc/hosts | xargs ping -c 1
+      HOSTNAME_IP=$(awk 'NR==NUM {print $1}' /etc/hosts)
+      ping -c 1 HOSTNAME_IP
+      echo "Test result for $HOSTNAME_IP is $RESULT at $TIME_NS"
 
-      i +=
+
+      NUM +=
       done
     sleep $TEST_PERIODICITY
 done
