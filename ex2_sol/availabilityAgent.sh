85ff495aeef529f58374efa69d43bdaabf9d2449
JonathanL <yonilevi0013@gmail.com>
2022-06-30 16:07:40 +0300

    ex1 solution - 7 retry

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,9 +1,16 @@
 TEST_PERIODICITY=5
 
+#HOST_NAME_IP=${awk 'NR==i {print $1}' /etc/hosts}
+
+
 while true
 do
-    # your implementation here
+    while [[ $HOST_NAME_IP != "" ]]
+    do
+      awk 'NR==i {print $1}' /etc/hosts | xargs ping -c 1
 
+      i +=
+      done
     sleep $TEST_PERIODICITY
 done
 
