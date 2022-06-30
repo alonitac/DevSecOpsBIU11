2d3ecedc6e72b332ba7a93fcd47fc7ceb5093fc4
danielvo <dannyvolk69@gmail.com>
2022-06-30 18:14:36 +0300

    ex2

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -10,7 +10,7 @@ do
           RESULT=1
       fi
       echo "Test result for $x is $RESULT at `data +%s%N`"
-      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$x value=$RESULT time=`date +%s%N`"
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$x value=$RESULT `date +%s%N`"
     done < hosts
     sleep $TEST_PERIODICITY
 done
