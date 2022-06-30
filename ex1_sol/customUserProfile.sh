82f89722b84d66a91247898ef86729776f4756e5
Sveta <savrekh@gmail.com>
2022-06-30 20:35:03 +0300

    ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -9,4 +9,7 @@ sudo touch .token
 
 FILE=$HOME/.token
 
-if `test -f $FILE` && [ `stat -c "%a" $FILE` != 600 ] ; then echo 'Warning: .token file has too open permissions' ; fi
+if `test -f $FILE` && [ `stat -c "%a" $FILE` != 600 ] ;
+  then
+  echo "Warning: .token file has too open permissions" ;
+fi
