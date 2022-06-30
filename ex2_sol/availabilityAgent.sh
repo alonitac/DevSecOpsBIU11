ae072b89ba529ceb3cb36249084a9f555756d813
DannyVol <69720882+DannyVol@users.noreply.github.com>
2022-06-30 16:02:33 +0300

    Update availabilityAgent.sh
    Fix Boolean issue

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -3,14 +3,12 @@ TEST_PERIODICITY=5
 while true
 do
     while read x; do
-      ping -c 1 $x &
-      if [ "$?" = "0" ]; then
+      ping -c 1 $x &> /dev/null 2>&1
+      RESULT=$?
+      if [[ "$RESULT" -nq "0" ]]; then
           RESULT=1
-      else
-          RESULT=0
       fi
       curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$x value=$RESULT `date +%s%N`"
     done < hosts
     sleep $TEST_PERIODICITY
 done
-
