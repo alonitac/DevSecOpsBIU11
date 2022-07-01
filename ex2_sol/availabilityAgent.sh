bd58d80e17e79fede0707669a76c78e4655b32f0
hohifrog <hohironyanai@gmail.com>
2022-07-01 18:10:44 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -4,7 +4,7 @@ TEST_PERIODICITY=5
 #Main loop
 while true; do
 #set variables
-filename='hosts'
+#filename='hosts'
 ErrorLevel=$?
 #Start ping loop
 while read l; do
@@ -14,7 +14,7 @@ then
     echo success
 fi
      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$l value=$ErrorLevel `date +%s%N`"
-done < $filename
+done < hosts
 sleep $TEST_PERIODICITY
 done
 #continue to optional on homework
