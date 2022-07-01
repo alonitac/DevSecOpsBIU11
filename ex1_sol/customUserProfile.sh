89cc47007bf79fab9d0868f521a25d61a074d07c
roee <roee.golan@outlook.com>
2022-07-01 08:56:06 +0300

    WIP

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,11 +1,6 @@
-# Get the aliases and functions
-if [ -f ~/.bashrc ]; then
-. ~/.bashrc
-fi
+#Mission1
+echo -e "****Hello $USER****"
 
-echo "****Hello $USER****"
-
-# User specific environment and startup programs
 PATH=$PATH:$HOME/bin
 
 export PATH
@@ -14,12 +9,16 @@ echo “##############################################################”
 echo ” Welcome to my linux station Mr `whoami` “
 echo “##############################################################”
 
-# displays the number of updates
+#Mission2-displays the number of updates:
+
 updates=$(/usr/lib/update-notifier/apt-check --human-readable)
 if [ ${updates:0:1} != 0 ]; then
    echo -e "$updates"
 fi
 
-if [[ -f $FILE && "stat -c "%a" $FILE" -ne 600  ]] ; then
-  echo "**Warning: .token file has too open permissions**"
+#Mission3-If file exists and permission is not 600:
+
+if [[ -f "$HOME/.token" && "$(stat -c "%a" $HOME/.token 2>/dev/null)" -ne '600' ]]
+then
+        echo -e  "\nWarning: .token file has too open permissions\n"
 fi
