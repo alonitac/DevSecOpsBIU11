3f745d5b502eb0f9ab6d68007503d58993432418
hohifrog <hohironyanai@gmail.com>
2022-07-01 22:16:08 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,37 +1,17 @@
-##!/bin/bash
-#
-#TEST_PERIODICITY=5
-##Main loop
-#while true; do
-##set variables
-#filename='hosts'
-#ErrorLevel=$?
-#n=1
-##Start ping loop
-#while read l; do
-#ping -c 1 $l
-#if [[ $ErrorLevel -eq 1 ]]; then
-#    echo success
-#else
-#    echo fail
-#fi
-#     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$l value=$ErrorLevel `date +%s%N`"
-#n=$((n+1))
-#done < $filename
-#sleep $TEST_PERIODICITY
-#done
-#continue to optional on homework
-TEST_PERIODICITY=5
+#!/bin/bash
 
-while true
-do
-    while read x; do
-      ping -c 1 $x &> /dev/null 2>&1
-      RESULT=$?
-      if [[ "$RESULT" -nq "0" ]]; then
-          RESULT=1
-      fi
-      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$x value=$RESULT `date +%s%N`"
-    done < hosts
-    sleep $TEST_PERIODICITY
+TEST_PERIODICITY=5
+while read l; do
+filename='hosts'
+ErrorLevel=$?
+ping -c 1 $l
+if [[ $ErrorLevel -eq 1 ]]; then
+    echo success
+else
+    echo fail
+fi
+     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$l value=$ErrorLevel `date +%s%N`"
+done < $filename
+sleep $TEST_PERIODICITY
 done
+#continue to optional on homework
