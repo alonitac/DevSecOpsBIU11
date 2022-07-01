1ef4823d51dbf7e895962669a00ca86a34eb13d7
hohifrog <hohironyanai@gmail.com>
2022-07-01 17:14:07 +0300

    ex2 solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
-
-while true
-do
-    # your implementation here
-
-    sleep $TEST_PERIODICITY
+#Main loop
+while true; do
+#set variables
+filename='hosts'
+ErrorLevel=$?
+#Start ping loop
+    echo Start
+while read line; do
+ping -c 1 $line
+if [[ $ErrorLevel -eq 0 ]]
+then
+    echo fail to run command: ping -c 1 $line
+else
+    echo success to run command: ping -c 1 $line
+fi
+    echo Test result for $line is $ErrorLevel at $(date +%s%N)
+done < $filename
+sleep $TEST_PERIODICITY
 done
-
-
-
+#continue to optional on homework
