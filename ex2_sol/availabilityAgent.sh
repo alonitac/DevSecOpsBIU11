88c46ae9753fe50fba7cc1774a6e69ee3ed632d2
Ogen <ogencohen98@gmail.com>
2022-07-01 16:44:14 +0300

    ex2 solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,28 @@
-TEST_PERIODICITY=5
+#!/bin/bash
 
+#run docker
+
+TEST_PERIODICITY=5
+IPFILE=~\ex2_sol\hosts
+TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+WORKING=1
+NOT_WORKING=0
 while true
 do
-    # your implementation here
+
+awk '{print $1}' < hosts | while read ip; do
+    if ping -c1 $ip >/dev/null 2>&1; then
+        echo 'test results for' $ip is $WORKING at $TIMESTAMP
+	curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ip value=$WORKING $TIMESTAMP"
+    else
+        echo 'test results for' $ip is $NOT_WORKING at $TIMESTAMP
+	curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ip value=$NOT_WORKING $TIMESTAMP"
+    fi
+
+
+done
 
     sleep $TEST_PERIODICITY
 done
-
 
 
