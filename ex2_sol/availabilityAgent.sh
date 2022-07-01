7372e4b2ce7bdc03226b67a82d120db2577cb348
hohifrog <hohironyanai@gmail.com>
2022-07-01 17:28:13 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -7,16 +7,13 @@ while true; do
 filename='hosts'
 ErrorLevel=$?
 #Start ping loop
-    echo Start
-while read line; do
-ping -c 1 $line
+while read l; do
+ping -c 1 $l
 if [[ $ErrorLevel -eq 0 ]]
 then
-    echo fail to run command: ping -c 1 $line
-else
-    echo success to run command: ping -c 1 $line
+    echo fail to run command: ping -c 1 $l
 fi
-    echo Test result for $line is $ErrorLevel at $(date +%s%N)
+    echo Test result for $l is $ErrorLevel at $(date +%s%N)
 done < $filename
 sleep $TEST_PERIODICITY
 done
