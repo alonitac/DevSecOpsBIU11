8c2ca91a76890f0bbfee5ee1a1c0161d15b340ca
hohifrog <hohironyanai@gmail.com>
2022-07-02 07:41:10 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -6,7 +6,7 @@ filename='hosts'
 RESULT=$?
 while read TESTED_HOST; do
 ping -c 1 "$TESTED_HOST" > /dev/null
-if [ $RESULT -eq 1 ]; then
+if [ $RESULT -eq 0 ]; then
         echo Test result for $TESTED_HOST is $RESULT at `date +%s%N`
 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT `date +%s%N`"
 fi
