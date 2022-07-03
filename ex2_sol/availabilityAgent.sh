98a8cb87aba456d022a803f99bf9971bb889cca7
chananri <riboh0301@gmail.com>
2022-07-03 15:17:58 +0300

    test

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,7 +2,18 @@ TEST_PERIODICITY=5
 
 while true
 do
-    # your implementation here
+  while read line
+  do
+        ping -c 1 $line &> /dev/null
+        if [ $? -eq 0 ]
+                then
+                RESULT=1
+        else
+                RESULT=0
+        fi
+        echo "Test result for $line is $RESULT at $(date +%s%N)"
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=$RESULT $(date +%s%N)"
+  done < hosts
 
     sleep $TEST_PERIODICITY
 done
