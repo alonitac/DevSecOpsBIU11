0d6932a6bac08ca8cf4dde5cc76c663b9c89511f
hohifrog <hohironyanai@gmail.com>
2022-07-03 23:52:45 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,10 +2,10 @@
 TEST_PERIODICITY=5
 while true;do
   for TESTED_HOST in $(cat ./hosts);do
-     TEST_TIMESTAMP=$(date +%s%N)
-      ping -c 1 -W 1 TESTED_HOST &> /dev/null 2>&1
+      TEST_TIMESTAMP=$(date +%s%N)
+      ping -c 1 TESTED_HOST &> /dev/null 2>&1
       if [ "$?" -eq 0 ];then RESULT=1;else RESULT=0;fi
-            echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+        echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done
 sleep $TEST_PERIODICITY;done
@@ -18,43 +18,3 @@ sleep $TEST_PERIODICITY;done
 
 
 
-#
-##!/bin/bash
-#
-#TEST_PERIODICITY=5
-#while true; do
-#filename='hosts'
-#RESULT=$?
-#while read hostname
-#do
-#    ping -c 1 -t 1 "$RESULT" > /dev/null 2>&1 &&
-#    if [ $ErrorLevel -eq 0 ]; then
-#    echo success
-#else
-#    echo fail
-#fi
-#    echo Test result for $output is $ErrorLevel at `date +%s%N`
-#curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP" `date +%s%N`"
-#done < 'hosts'
-sleep $TEST_PERIODICITY
-done
-
-##!/bin/bash
-#
-#TEST_PERIODICITY=5
-#while true; do
-#filename="hosts"
-#ErrorLevel=$?
-#while IFS='' read -r l || [[ -n "$l" ]]; do
-#ping -c 1 $l
-#if [[ $ErrorLevel -eq 1 ]]; then
-#    echo success
-#else
-#    echo fail
-#fi
-#curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$l value=$ErrorLevel `date +%s%N`"
-#done < "$filename"
-#sleep $TEST_PERIODICITY
-#done
-##continue to optional on homework
-# echo "Checking $LINE"
