17b10d71d81ca6d45eee909228fec63dbad2ac75
hohifrog <hohironyanai@gmail.com>
2022-07-03 21:01:02 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,29 +1,51 @@
-#!/bin/bash
+
 
 TEST_PERIODICITY=5
 
-while true; do
+while true
+do
+    # your implementation here
+    for TESTED_HOST in $(cat ./hosts)
+    do
+        TEST_TIMESTAMP=$(date +%s%N)
+        ping -c 1 -W 1 $TESTED_HOST &> /dev/null
+        if [ "$?" -eq 0 ]; then RESULT=1; else RESULT=0; fi
+        echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
 
-  RESULT=$?
-  TS=$(date +%s%N)
-  for TESTED_HOST in $(cat ./hosts); do #thanks to yitav!!!!!!! i went with while and not for!!!!!
-  ping -c 1 $TESTED_HOST > /dev/null
-  if [ $RESULT -eq 0 ]; then
-    RESULT=1
-  else
-    RESULT=0
-  fi
-#print results
-     echo Test result for $TESTED_HOST is $RESULT at $TS
-#influxdb
-  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TS"
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+
+    done
+
+
+
+    sleep $TEST_PERIODICITY
 done
-
-sleep $TEST_PERIODICITY
-
-done
-
-
+##!/bin/bash
+#
+#TEST_PERIODICITY=5
+#
+#while true; do
+#
+#  RESULT=$?
+#  TS=$(date +%s%N)
+#  for TESTED_HOST in $(cat ./hosts); do #thanks to yitav!!!!!!! i went with while and not for!!!!!
+#  ping -c 1 $TESTED_HOST > /dev/null
+#  if [ $RESULT -eq 0 ]; then
+#    RESULT=1
+#  else
+#    RESULT=0
+#  fi
+##print results
+#     echo Test result for $TESTED_HOST is $RESULT at $TS
+##influxdb
+#  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TS"
+#done
+#
+#sleep $TEST_PERIODICITY
+#
+#done
+#
+#
 
 
 
