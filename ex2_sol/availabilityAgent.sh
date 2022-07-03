aef55361bdb58df862ceea2c87ec647053eac030
simon <simonshmo@gmail.com>
2022-07-03 22:36:04 +0300

    ex1_test1

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,24 +1,21 @@
 
 TEST_PERIODICITY=5
+
 while true
 do
-    while read TESTED_HOST
-
+  while read line
   do
-
-    ping -c 1 -W 1 $TESTED_HOST &> /dev/null
-
-   if [ "$?" -eq 0 ]; then
-        RESULT=0
-   else
-        RESULT=1
-   fi
-
-    echo "Test result for $TESTED_HOST is $RESULT at $(date +%s%N)"
-    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $(date +%s%N)"
-done  < hosts
+        ping -c 1 -W 1 $line &> /dev/null
+        if [ $? -eq 0 ]
+                then
+                RESULT=1
+        else
+                RESULT=0
+        fi
+        echo "Test result for $line is $RESULT at $(date +%s%N)"
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=$RESULT $(date +%s%N)"
+  done < hosts
 
     sleep $TEST_PERIODICITY
-
 done
 
