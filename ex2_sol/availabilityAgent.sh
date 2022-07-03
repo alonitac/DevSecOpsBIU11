31e425a727ebfcbb074a2ea24637087f30a0da52
OrC <cohenitsme@gmail.com>
2022-07-03 10:54:58 +0300

    sol for ex 2

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,18 +2,17 @@ TEST_PERIODICITY=5
 
 while true
 do
-
-while read PC; do
+while read TESTED_HOST; do
   if [[ ! -z "$PC" ]]
   then
       ping -c 1 "${PC}" >/dev/null
-      TEST_SUCC=($?)
-      TIME_EP=$(date +%s%N)
-      echo "Test result for ${PC} is ${TEST_SUCC} at ${TIME_EP}"
-      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=${PC} value=${TEST_SUCC} ${TIME_EP}"
+      RESULT=($?)
+      TEST_TIMESTAMP=$(date +%s%N)
+      echo "Test result for ${TESTED_HOST} is ${RESULT} at ${TEST_TIMESTAMP}"
+      #curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=${PC} value=${TEST_SUCC} ${TIME_EP}"
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   fi
 done <~hosts
-
     sleep $TEST_PERIODICITY
 done
 
