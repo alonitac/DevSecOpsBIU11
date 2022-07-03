25c8c66b131187582a5dcc14835e8ffeaa52f078
EidoB <eidobennun@gmail.com>
2022-07-04 00:04:28 +0300

    commit message

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,25 @@
-TEST_PERIODICITY=5
 
+#!/bin/bash
+TEST_PERIODICITY=5
 while true
 do
-    # your implementation here
 
-    sleep $TEST_PERIODICITY
+	while read line
+	do
+		ping $line -c 1 &>/dev/null
+		if [ $? -eq 0 ]
+		then
+			echo Test resulf for $line is 1 at $(date +%s)
+			curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=$? $(date +%s)"
+		else
+			echo Test result for $line is 0 at $(date +%s)
+			curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=$? $(date +%s)"
+
+
+		fi
+
+
+	done <hosts
+
+	sleep $TEST_PERIODICITY
 done
-
-
-
