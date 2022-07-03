87b2180e1431aedc423a6e67c8096b4795b88833
refaelsa <refael32901@gmail.com>
2022-07-03 08:06:16 +0300

    ex2 solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
 TEST_PERIODICITY=5
 
 while true
 do
-    # your implementation here
-
-    sleep $TEST_PERIODICITY
+while read line; do
+        ping -D $line -c  1 1>/dev/null
+        EC=$?
+        TEST_TIMESTAMP=$(date +%s%N)
+                if [  "$EC" -eq "0" ]; then
+                echo Test result for ${line} is 1 at $TEST_TIMESTAMP
+                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=1 $TEST_TIMESTAMP"
+                else
+                echo Test result for ${line} is 0 at $TEST_TIMESTAMP
+                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=0 $TEST_TIMESTAMP"
+                fi
+done < hosts
+echo
+sleep $TEST_PERIODICITY
 done
-
 
 
