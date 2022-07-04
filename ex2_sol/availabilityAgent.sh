0a201cc8fad0d0c4e81fb5ddd88e05911d6053ff
Igal Vexler <igalv@traiana.com>
2022-07-04 17:50:25 +0300

    ex2 solution - Igal Vexler

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -5,20 +5,17 @@ TEST_PERIODICITY=5
 while true
 do
     # your implementation here
-    FILENAME="hosts"
-    HOSTS_LINES=$(cat $FILENAME)
-
-    for TESTED_HOST in $HOSTS_LINES # iterate over the lines of file hosts
+    while read TESTED_HOST # iterate over the lines of file hosts
     do
-        TEST_TIMESTAMP=$(date +%s%N) # returns the number of nanoseconds
-        ping -c 1 -W 1 "$HOSTS_LINES" &> /dev/null # single ping for each TESTED_HOST (line) with timeout of 1 sec
+        ping -c 1 -W 1 "$TESTED_HOST" &> /dev/null # single ping for each TESTED_HOST (line) with timeout of 1 sec
             if [ "$?" -eq 0 ] # check exit status of a ping
                then RESULT=1
             else RESULT=0
             fi
+        TEST_TIMESTAMP=$(date +%s%N) # returns the number of nanoseconds
         echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
         curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-    done
+    done < hosts
 
     sleep $TEST_PERIODICITY
     echo
