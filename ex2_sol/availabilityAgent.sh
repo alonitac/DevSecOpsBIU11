0af12f2a6dc0562e696c18cbbcd7351ceb729b36
Yehuda Dahan <62338718+JudaD@users.noreply.github.com>
2022-07-04 14:00:17 +0300

    ex2_sol agent

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
+TIME=$(date +%s.%N)
 
 while true
 do
-    # your implementation here
-
-    sleep $TEST_PERIODICITY
+	while read line; do
+	  ping -c 1 -W 1 $line &> /dev/null
+		  if [ $? -eq 0 ]; then
+	    RESULT=1
+	  	else
+	  	RESULT=0
+	  	fi
+  	  echo "Test result for $line is $RESULT at $TIME"
+  done < hosts
+sleep $TEST_PERIODICITY
 done
-
-
-
