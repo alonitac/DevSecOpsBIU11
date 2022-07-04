722bce95504913f6ca30f3bb186ee153a567f359
xejnmthtc <107956419+xejnmthtc@users.noreply.github.com>
2022-07-04 23:13:54 +0300

    Update availabilityAgent.sh

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,36 @@
+Created by anan - DevSecOpsBIU11
+#!/bin/bash
+# Program name: pingall.sh
+# Tested on local machine #
+
+TESTED_HOST=/home/anan/exam/exam2/host
 TEST_PERIODICITY=5
+TEST_TIMESTAMP=$(date +%s%N)
+
 
 while true
 do
-    # your implementation here
 
-    sleep $TEST_PERIODICITY
+for ip in $(cat $TESTED_HOST)
+do
+
+        echo "Ping in progress"
+        ping $ip -c 1 $TESTED_HOST &> /dev/null
+
+        if [ "$?" -eq "0" ]; then
+        echo Test result for "${myArray[@]}" = $ip is 0 at ${TEST_TIMESTAMP}
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=0 $TEST_TIMESTAMP"
+
+else
+        echo Test result for "${myArray[@]}" = $ip is 1 at ${TEST_TIMESTAMP}
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=1 $TEST_TIMESTAMP"
+        fi
+
+
+done < host
+
+
+
+
+sleep $TEST_PERIODICITY
 done
-
-
-
