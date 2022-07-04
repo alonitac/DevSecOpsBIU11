0c3f27be159b40606ebec3098e63da61629b058c
hohifrog <hohironyanai@gmail.com>
2022-07-04 18:08:58 +0300

    ex2_solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -7,13 +7,14 @@ while true
       do
         TEST_TIMESTAMP=$(date +%s%N)
         ping -c 1 TESTED_HOST &> /dev/null
-        if [ "$?" -eq 0 ]
+        if [[ "$?" -eq 0 ]]
         then RESULT=1
         else RESULT=0
         fi
         echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done
+
 
 sleep $TEST_PERIODICITY;
 done
