413ecd5f2409b7dbbc4a46815179b66ed2484159
JonathanL <yonilevi0013@gmail.com>
2022-07-04 18:41:18 +0300

    WIP

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 TEST_PERIODICITY=5
-
 HOST_FILE=/home/jonathan/hosts.txt
-
+NANOSECONDS=1000000
 
 while true
 do
         while read host
         do
-                RESULTS=$(ping -c 1 ${host})
-                TIME=$(awk 'BEGIN { FS="=" } {print $3}' $RESULTS)
-                if [[ $? != 1 ]]; then
-                        echo Test result for ${host} is $TIME
+                ping -c 1 ${host} > /dev/null
+                if [[ $? == 0 ]]; then
+                        #TIME=$(ping -c 1 ${host} > /dev/null | awk -F '[=]' '/64/ {print $4}' | awk '{print $1}')
+                        echo Test result for ${host} is $? at $(( ($( date +%s%N)/1000000 )))
                 else
-                        echo Failed
+                        echo Test result for ${host} is 1 at $(( ($( date +%s%N)/1000000 )))
                 fi
         done < hosts.txt
     sleep $TEST_PERIODICITY
 done
+
