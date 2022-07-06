b4b68ca6bf923ee69ce3ad5ea96b57b851bf4f18
igor_stadnyk <igor.stadnyk@wiliot.com>
2022-07-06 10:40:39 +0300

    ex2 solution fix3

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -14,7 +14,7 @@ do
 TEST_TIMESTAMP=$(date +%s%N)
 echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-done < hosts
+done
 
 
 
