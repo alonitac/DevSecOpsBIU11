75e947cb1c95d189e6fb98025da4dc38bcb9dcb6
igor_stadnyk <igor.stadnyk@wiliot.com>
2022-07-06 07:40:57 +0300

    ex2 solution fix

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,6 +2,7 @@ TEST_PERIODICITY=5
 
 while true
 do
+
 for TESTED_HOST in $HOSTS
 do
  bb=$(ping -c 1 -W 1 $TESTED_HOST | grep 'transmitted,' | awk '{print $4}')
@@ -10,10 +11,15 @@ do
      else
        RESULT=0
       fi
-TEST_TIMESTAM=$(date +%s%N)
-echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAM"
+TEST_TIMESTAMP=$(date +%s%N)
+echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
 done
+
+
+
+
+
 
     sleep $TEST_PERIODICITY
 done
