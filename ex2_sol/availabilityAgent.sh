9c05b3eba3688b952ccf302459eb8a95a0d3501b
hohifrog <hohironyanai@gmail.com>
2022-07-06 20:34:44 +0300

    ex2_solution (Deleted a duplicate command)

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -11,14 +11,9 @@ while true;do
       echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
       curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
     done
-sleep $TEST_PERIODICITY;
-
+sleep $TEST_PERIODICITY
 done
 
-
-
-sleep $TEST_PERIODICITY;
-done
 
 
 
