08381a42698a54d5a8a35c9478f8cd56b482bb0e
JonathanL <yonilevi0013@gmail.com>
2022-07-07 11:47:50 +0300

    solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -3,19 +3,24 @@
 TEST_PERIODICITY=5
 HOST_FILE=/home/jonathan/hosts.txt
 NANOSECONDS=1000000
+TEST_TIMESTAMP=$(( ($( date +%s%N)/$NANOSECONDS )))
+
 
 while true
 do
         while read host
         do
-                ping -c 1 ${host} > /dev/null
-                if [[ $? == 0 ]]; then
-                        #TIME=$(ping -c 1 ${host} > /dev/null | awk -F '[=]' '/64/ {print $4}' | awk '{print $1}')
-                        echo Test result for ${host} is $? at $(( ($( date +%s%N)/1000000 )))
+                ping -c 1 ${host} > /dev/null 2>&1
+                if      [[ $? == 0 ]] ; then
+                        RESULT=1
                 else
-                        echo Test result for ${host} is 1 at $(( ($( date +%s%N)/1000000 )))
+                        RESULT=0
                 fi
+                        echo Test result for ${host} is $RESULT at $TEST_TIMESTAMP
+                        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=${host} value=$RESULT $TEST_TIMESTAMP"
         done < hosts.txt
+        echo
     sleep $TEST_PERIODICITY
 done
+
 
