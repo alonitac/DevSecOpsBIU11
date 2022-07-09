651809a72ae4c786db6a04e260479d311508aaef
maor weiss <maorweissb@gmail.com>
2022-07-10 00:01:37 +0300

    ex2 solution # Until mission number 4

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,10 +2,32 @@ TEST_PERIODICITY=5
 
 while true
 do
-    # your implementation here
+  # Mission1
+  while read TESTED_HOST; do
+    # Mission2
+    # Ping a host and check if the host is unreachable.
+    # If it's unreachable, put a result on the '/tmp/availabilityAgent_result' file.
+    ping -c 1 "$TESTED_HOST"  | grep 'Unreachable' > /tmp/availabilityAgent_result 2>/dev/null
+    if [[ -s /tmp/availabilityAgent_result ]]; then
+      # If there is (Which means 'Unreachable'), RESULT equal to 0. Else, RESULT equal to 1.
+      RESULT=0
+    else
+      RESULT=1
+    fi
 
-    sleep $TEST_PERIODICITY
+    # Reset the '/tmp/availabilityAgent_result' file.
+    sudo cat /dev/null > /tmp/availabilityAgent_result
+
+    # Get the timestamp in nanoseconds and put it to a variable.
+    TEST_TIMESTAMP="$(date +%s%N)"
+
+    # Mission 3
+    echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+    # Mission 4
+    # After installing the influxdb container and creating the 'hosts_metrics' DB
+    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+
+  done <hosts
+
+  sleep "$TEST_PERIODICITY"
 done
-
-
-
