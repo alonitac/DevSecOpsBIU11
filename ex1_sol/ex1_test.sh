45d2df256b4287ea0b6241c040da03842b201199
Yarin Paryente <yarinpariente10@gmail.com>
2022-07-10 02:51:24 +0300

    as

--- ex1_sol/ex1_test.sh
+++ ex1_sol/ex1_test.sh
@@ -1,4 +1,5 @@
-set -e
+
+++set -e
 
 # run student solution script
 chmod +x ./customUserProfile.sh
--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,9 +2,22 @@ TEST_PERIODICITY=5
 
 while true
 do
-    # your implementation here
-
+  while read ping;
+    do
+        ping $ping -c 1 -W 1 &> /dev/null
+        if [ $? -eq 0 ];
+        then
+                RESULT=1
+                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
+                echo "The result for $ping is $RESULT at $(date +%s%N)"
+        else
+                RESULT=0
+                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
+                echo "The result for $ping is $RESULT at $(date +%s%N)"
+        fi
+     done < host.txt
     sleep $TEST_PERIODICITY
+    echo
 done
 
 
