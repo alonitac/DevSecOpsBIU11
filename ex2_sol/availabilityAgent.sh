dd74beb669d65ebf53458dbda3700836b8bfaf31
Yarin Paryente <yarinpariente10@gmail.com>
2022-07-10 20:45:14 +0300

    update

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,24 +1,21 @@
-TEST_PERIODICITY=5
+#!/bin/bash
 
+TEST_PERIODICITY=5
 while true
 do
-  while read ping;
+  while read line
     do
-        ping $ping -c 1 -W 1 &> /dev/null
-        if [ $? -eq 0 ];
+        ping -c 1 -W 1 $line &> /dev/null
+        if [ $? -eq 0 ]
         then
                 RESULT=1
                 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
-                echo "The result for $ping is $RESULT at $(date +%s%N)"
+                echo "The result for $line is $RESULT at $(date +%s%N)"
         else
                 RESULT=0
                 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
-                echo "The result for $ping is $RESULT at $(date +%s%N)"
+                echo "The result for $line is $RESULT at $(date +%s%N)"
         fi
-     done < host.txt
+    done < hosts
     sleep $TEST_PERIODICITY
-    echo
 done
-
-
-
