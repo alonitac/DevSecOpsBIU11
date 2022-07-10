96cb3fdbfd2b99938a924ff10f4668f196dd2c37
Yarin Paryente <yarinpariente10@gmail.com>
2022-07-10 20:47:57 +0300

    update

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -9,13 +9,11 @@ do
         if [ $? -eq 0 ]
         then
                 RESULT=1
-                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
-                echo "The result for $line is $RESULT at $(date +%s%N)"
         else
                 RESULT=0
-                curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$ping value=$RESULT $(date +%s%N)"
-                echo "The result for $line is $RESULT at $(date +%s%N)"
         fi
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$line value=$RESULT $(date +%s%N)"
+        echo "The result for $line is $RESULT at $(date +%s%N)"
     done < hosts
     sleep $TEST_PERIODICITY
 done
