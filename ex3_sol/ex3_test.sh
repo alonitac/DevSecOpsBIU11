28fd3edadda0b67c81a10a4820fb2288e50913ee
alonit <alon.itach@mail.huji.ac.il>
2022-07-12 11:35:20 +0300

    fix

--- ex3_sol/ex3_test.sh
+++ ex3_sol/ex3_test.sh
@@ -30,7 +30,7 @@ echo 'Execute: ./seat.sh lock "Oedipus_the_King" "Andreas" 56'
 TEXT=$(./seat.sh lock "Oedipus_the_King" "Andreas" 56)
 
 if ! echo "$TEXT" | grep -q "Seat was locked"; then
-  echo "Expected 'Seat was locked' but found \'$TEXT\'"
+  echo "Expected 'Seat was locked' but found '$TEXT'"
   exit 1
 fi
 
@@ -38,7 +38,7 @@ echo 'Execute: ./seat.sh lock "Oedipus_the_King" "Marios" 56'
 TEXT=$(./seat.sh lock "Oedipus_the_King" "Marios" 56)
 
 if ! echo "$TEXT" | grep -q "This seat is currently locked by other customer, try again later"; then
-  echo "Expected 'This seat is currently locked by other customer, try again later' but found \'$TEXT\'"
+  echo "Expected 'This seat is currently locked by other customer, try again later' but found '$TEXT'"
   exit 1
 fi
 
@@ -46,7 +46,7 @@ echo 'Execute: ./seat.sh release "Oedipus_the_King" "Andreas" 56'
 TEXT=$(./seat.sh release "Oedipus_the_King" "Andreas" 56)
 
 if ! echo "$TEXT" | grep -q "Seat was released"; then
-  echo "Expected 'Seat was released' but found \'$TEXT\'"
+  echo "Expected 'Seat was released' but found '$TEXT'"
   exit 1
 fi
 
@@ -54,7 +54,7 @@ echo 'Execute: ./seat.sh book "Oedipus_the_King" "Marios" 56'
 TEXT=$(./seat.sh book "Oedipus_the_King" "Marios" 56)
 
 if ! echo "$TEXT" | grep -q "Booking failed, lock your seat before"; then
-  echo "Expected 'Booking failed, lock your seat before' but found \'$TEXT\'"
+  echo "Expected 'Booking failed, lock your seat before' but found '$TEXT'"
   exit 1
 fi
 
@@ -62,7 +62,7 @@ echo 'Execute: ./seat.sh lock "Oedipus_the_King" "Marios" 56'
 TEXT=$(./seat.sh lock "Oedipus_the_King" "Marios" 56)
 
 if ! echo "$TEXT" | grep -q "Seat was locked"; then
-  echo "Expected 'Seat was locked' but found \'$TEXT\'"
+  echo "Expected 'Seat was locked' but found '$TEXT'"
   exit 1
 fi
 
@@ -70,7 +70,7 @@ echo 'Execute: ./seat.sh book "Oedipus_the_King" "Marios" 56'
 TEXT=$(./seat.sh book "Oedipus_the_King" "Marios" 56)
 
 if ! echo "$TEXT" | grep -q "Successfully booked this seat!"; then
-  echo "Expected 'Successfully booked this seat!' but found \'$TEXT\'"
+  echo "Expected 'Successfully booked this seat!' but found '$TEXT'"
   exit 1
 fi
 
@@ -78,7 +78,7 @@ echo 'Execute: ./seat.sh lock "Oedipus_the_King" "Andreas" 56'
 TEXT=$(./seat.sh lock "Oedipus_the_King" "Andreas" 56)
 
 if ! echo "$TEXT" | grep -q "Locking failed, seat is already booked"; then
-  echo "Expected 'Locking failed, seat is already booked' but found \'$TEXT\'"
+  echo "Expected 'Locking failed, seat is already booked' but found '$TEXT'"
   exit 1
 fi
 
