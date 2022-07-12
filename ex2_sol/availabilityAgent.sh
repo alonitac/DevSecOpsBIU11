adcf71d9680af09bf20c386764c40a2cb447bd90
arielfcotrim <arielforner@gmail.com>
2022-07-12 21:08:15 +0300

    ex1 solution (final no hard coding)

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
+PING_TIMESTAMP=$(date +%N)
+EXIT_CODE=$?
 
 while true
 do
-    # your implementation here
+  for HOST_OR_IP in $(cat ./hosts); do
+    ping -c 1 "$HOST_OR_IP" &> /dev/null
 
-    sleep $TEST_PERIODICITY
+    if [[ EXIT_CODE -eq 0 ]]
+    then
+      RETURN_CODE=1
+    else
+      RETURN_CODE=0
+    fi
+
+  echo "Test result for $HOST_OR_IP is $RETURN_CODE at $PING_TIMESTAMP"
+  done
+
+  echo ""
+  sleep $TEST_PERIODICITY
 done
-
-
-
