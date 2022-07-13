8f825fc6d8fa43194a05dda0a202e2b9282217c2
arielfcotrim <arielforner@gmail.com>
2022-07-13 21:51:15 +0300

    ariel_ex2_sol_EC

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 TEST_PERIODICITY=5
-EXIT_CODE=$?
+#EXIT_CODE=$?
 
 while true
 do
   while read HOST_OR_IP
     do
       PING_TIMESTAMP=$(date +%s%N)
+
       ping -c 1 -W 1 $HOST_OR_IP &> /dev/null
 
-      if [[ EXIT_CODE -eq 0 ]]
+      if [[ $? -eq 0 ]]
       then
         RETURN_CODE=1
       else
         RETURN_CODE=0
       fi
 
-      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$HOST_OR_IP value=$RETURN_CODE $PING_TIMESTAMP"
       echo "The result for $HOST_OR_IP is $RETURN_CODE at $PING_TIMESTAMP"
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$HOST_OR_IP value=$RETURN_CODE $PING_TIMESTAMP"
     done < hosts
     sleep $TEST_PERIODICITY
 done
