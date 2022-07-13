9f87703d3950e496493511b250a7e577d31e335a
maor weiss <maorweissb@gmail.com>
2022-07-13 18:42:50 +0300

    ex2 solution # Set while loop outside the ping_to_host function for parallel movement

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,40 +1,41 @@
 TEST_PERIODICITY=5
 
 function ping_to_host {
-  # Mission1
-    while read TESTED_HOST; do
-    # Ping a host in background. If successful, RESULT will be equal to 1, else to 0
-    # For ping, call once for dynamically timeout duration. To TEST_PERIODICITY sec
-    ping $TESTED_HOST -c 1 -W 1 &> /dev/null
-    if [[ $? -ne 0 ]]; then
-      RESULT=0
-    else
-      RESULT=1
-    fi
+  TESTED_HOST=$1
+  # Mission 2
+  # Ping a host in background. If successful, RESULT will be equal to 1, else to 0
+  # For ping, call once for dynamically timeout duration. To TEST_PERIODICITY sec
+  ping $TESTED_HOST -c 1 -W $TEST_PERIODICITY &> /dev/null
+  if [[ $? -ne 0 ]]; then
+    RESULT=0
+  else
+    RESULT=1
+  fi
 
-    # Get the timestamp in nanoseconds and put it to a variable
-    TEST_TIMESTAMP="$(date +%s%N)"
+  # Get the timestamp in nanoseconds and put it to a variable
+  TEST_TIMESTAMP="$(date +%s%N)"
 
-    # Mission 3 + Mission 7
-    PING_LATENCY="$(ping $TESTED_HOST -c1 | head -n 2 | tail -n 1 | echo "$(awk 'BEGIN {FS="[=]|ms"} {print $4}')sec")"
-    # If there is no result, output 0
-    if [[ $PING_LATENCY = "sec" ]]
-    then
-      PING_LATENCY=0
-    fi
+  # Mission 3 + Mission 7
+  PING_LATENCY="$(ping $TESTED_HOST -c1 | head -n 2 | tail -n 1 | echo "$(awk 'BEGIN {FS="[=]|ms"} {print $4}')sec")"
+  # If there is no result, output 0
+  if [[ $PING_LATENCY = "sec" ]]
+  then
+    PING_LATENCY=0
+  fi
 
-    echo "Test result for $TESTED_HOST is $PING_LATENCY at $TEST_TIMESTAMP"
+  echo "Test result for $TESTED_HOST is $PING_LATENCY at $TEST_TIMESTAMP"
 
-    # Mission 4
-    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-  done <hosts
+  # Mission 4
+  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
 }
 
 while true
 do
-  # Mission 6 - Use the & operator to run all tests in parallel
-  ping_to_host &
-
+  # Mission1
+  while read TESTED_HOST; do
+    # Mission 6 - Use the & operator to run all tests in parallel
+    ping_to_host $TESTED_HOST &
+  done <hosts
   # The loop body will be executed every TEST_PERIODICITY seconds (5 seconds in our case).
   sleep "$TEST_PERIODICITY"
 
