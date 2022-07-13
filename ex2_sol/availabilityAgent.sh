761112f60eaf9911543abb0a21faa848d435db65
arielfcotrim <arielforner@gmail.com>
2022-07-13 19:33:02 +0300

    ariel_ex2_sol(fix i hope)

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,25 +2,21 @@
 
 TEST_PERIODICITY=5
 
+TEST_TIMESTAMP=$(date +%N)
 EXIT_CODE=$?
 
-
-
 while true; do
-  for TESTED_HOST in $(cat ./hosts); do
-      TEST_TIMESTAMP=$(date +%s%N)
-      ping -c 1 -W 1 $TESTED_HOST &> /dev/null
-
-      if [[ EXIT_CODE -eq 0 ]]
-      then
-        RESULT=0
-      else
+  while read -r hosts; do
+      ping -c 1 -W 1 "${TESTED_HOST}" &> /dev/null
+      if [[ EXIT_CODE -eq 0 ]]; then
         RESULT=1
+      else
+        RESULT=0
       fi
 
     echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done
 
-sleep $TEST_PERIODICITY
+  sleep $TEST_PERIODICITY
 done
