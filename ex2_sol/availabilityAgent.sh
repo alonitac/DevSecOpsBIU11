b5dc84567ea67cbb1c9a9acb14c0de206de0ba4f
arielfcotrim <arielforner@gmail.com>
2022-07-13 21:40:26 +0300

    ariel_ex2_solHOIP2

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -6,11 +6,11 @@ do
   while read HOST_OR_IP
     do
         ping -c 1 -W 1 $HOST_OR_IP &> /dev/null
-        if [ $? -eq 0 ]
+        if [[ $? -eq 0 ]]
         then
-                RESULT=1
+          RESULT=1
         else
-                RESULT=0
+          RESULT=0
         fi
         curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$HOST_OR_IP value=$RESULT $(date +%s%N)"
         echo "The result for $HOST_OR_IP is $RESULT at $(date +%s%N)"
