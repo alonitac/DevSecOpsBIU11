b2e24f3b6d95404af9ffc78caa38a80fe9199c50
arielfcotrim <arielforner@gmail.com>
2022-07-13 21:16:45 +0300

    ariel_ex2_sol(fix i hope) :(

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -2,19 +2,16 @@
 
 TEST_PERIODICITY=5
 
-SEND_PING="ping -c 1"
 TEST_TIMESTAMP=$(date +%N)
 
 while true
 do
   for TESTED_HOST in $(cat ./hosts); do
-    $SEND_PING $TESTED_HOST &> /dev/null
+    ping -c 1 -W 1 $TESTED_HOST &> /dev/null
 
     if [[ "$?" -eq 0 ]]
-    then
-      RESULT=1
-    else
-      RESULT=0
+    then RESULT=1
+    else RESULT=0
     fi
 
     echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
