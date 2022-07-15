88d479302ad4e6bdb4d5cc8dd1aa6260f54d013d
yitav <yitavel@hotmail.com>
2022-07-15 19:29:14 +0300

    ex3 solution

--- ex3_sol/seat.sh
+++ ex3_sol/seat.sh
@@ -1,6 +1,14 @@
 LOCK_TTL=600
 HALL_CAPACITY=300
 
+function checkSeatNotExceed {
+  local seat=$1
+
+  if [[ "$seat" -gt "$HALL_CAPACITY" || "$seat" -le 0 ]]; then
+        exit 5
+  fi
+
+}
 # This helper can be used to communicate with Redis
 # Usage example:
 #     redis-do set mykey myval EX 6
@@ -19,12 +27,25 @@ function redis-do {
 #
 # Note that seat number must not exceed HALL_CAPACITY, otherwise the program exits with status 5
 function lock {
+
   local show=$1
   local name=$2
   local seat=$3
 
   # your implementation here ...
+  checkSeatNotExceed $3
 
+  if redis-do "exists book-$show-$seat" | grep -q "1"; then
+        echo "Locking failed, seat is already booked"
+
+  elif redis-do "setnx lock-$show-$seat $name" | grep -q "1"; then
+	redis-do "EXPIRE lock-$show-$seat $LOCK_TTL" >> /dev/null
+	echo "Seat was locked"
+#if setnx failed then the lock is taken - check if it is taken ny someone else:
+  elif [[ $(redis-do "get lock-$show-$seat") != "$name" ]]; then
+        echo "This seat is currently locked by other customer, try again later"
+
+  fi
 }
 
 # This function book a name ($2, string) and seat ($3, integer) in show ($1, string).
@@ -39,7 +60,15 @@ function book {
   local seat=$3
 
   # your implementation here ...
+  checkSeatNotExceed $3
 
+  if [[ $(redis-do "get lock-$show-$seat") = "$name" ]]; then
+	if redis-do "setnx book-$show-$seat $name" | grep -q "1"; then
+ 		echo "Successfully booked this seat!"
+	fi
+  else
+	echo "Booking failed, lock your seat before"
+  fi
 }
 
 
@@ -54,7 +83,13 @@ function release {
   local seat=$3
 
   # your implementation here ...
+  checkSeatNotExceed $3
 
+  if [[ $(redis-do "get lock-$show-$seat") = "$name" ]]; then
+	if redis-do "del lock-$show-$seat" | grep -q "1"; then
+                echo "Seat was released"
+        fi
+  fi
 }
 
 
@@ -64,7 +99,10 @@ function reset {
   local show=$1
 
   # your implementation here ...
-
+  for seat in $(seq 1 $HALL_CAPACITY)
+  do
+	redis-do "del lock-$show-$seat"
+  done
 }
 
 
