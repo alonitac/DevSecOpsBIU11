63f4e1b00514fe0fa7151e45d389ebd8efac0b20
eran <eranjunk88@gmail.com>
2022-07-15 09:10:11 +0300

    ex2_test

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,8 +1,22 @@
 TEST_PERIODICITY=5
+TEST_TIMESTAMP=$(date +%s%N)
 
 while true
 do
     # your implementation here
+    #While loop iteration - Read IP Address from hosts file and present the test result date in nanoseconds
+    while read TESTED_HOST; do
+        ping -c 1 $TESTED_HOST > /dev/null
+        if [[ $? == 0 ]]; then
+          RESULT=1
+        else
+          RESULT=0
+                  fi
+        echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+      done <hosts
+    #end of loop
+    
 
     sleep $TEST_PERIODICITY
 done
