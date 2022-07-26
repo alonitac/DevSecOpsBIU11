fd43069244d3174f5972df1773e0ed6668c8ec09
Ella Benis <ella@benis.co.il>
2022-07-26 11:50:56 +0300

    ex2 answer !

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
- # Testing!
+
 TEST_PERIODICITY=5
 
-#
 while true
 do
-  for HOST_OR_IP in $(cat ./hosts); do
-    PING_TIMESTAMP=$(date +%s%N)
+    # your implementation here
 
-    ping -c 1 -W 1 $HOST_OR_IP &> /dev/null
-    if [[ $? -eq 0 ]]
-    then
-      RETURN_CODE=1
+  while read host
+  do
+    ping -c 1 -W 1 $host &> /dev/null
+    if [[ $? -eq 0]]
+      then
+      RESULT=1
     else
-      RETURN_CODE=0
-    fi
+      RESULT=0
+   fi
+    echo "Test result for $host is "$result" at $(date +%s%N)"
+    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$host value=$RESULT $(date +%s%N)"
 
-    echo "The result for $HOST_OR_IP is $RETURN_CODE at $PING_TIMESTAMP"
-    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$HOST_OR_IP value=$RETURN_CODE $PING_TIMESTAMP"
-  done
+  done < hosts
+
 
   sleep $TEST_PERIODICITY
 done
