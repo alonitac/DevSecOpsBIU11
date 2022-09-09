41236a1f8d45398b42f7d3f5301cf149f26c0721
alonit <alon.itach@mail.huji.ac.il>
2022-09-09 12:31:13 +0300

    my changes

--- ex2_sol/availabilityAgent-solution.sh
+++ ex2_sol/availabilityAgent-solution.sh
@@ -6,13 +6,20 @@ do
       RESULT=0
       if ping -c 1 -W 2 "$TESTED_HOST"
       then
-        RESULT=1
+        RESULT=9
       fi
-      TEST_TIMESTAMP="$(date +%s%N)"
+      TEST_TIMESTAMP="$(date +%s%s%N)"
+
+      X=9
+      echo $X
 
       echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
-      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+      curl -X POST 'http://localhost:8087/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done < hosts
 
   sleep "$TEST_PERIODICITY"
 done
+
+fucntion foo {
+  x=9
+}
